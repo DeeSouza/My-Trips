b44ebe9dc5cef5f34a1314ef3396df69bb5d726e
Diego Souza <dee-souza@hotmail.com>
2022-12-26 15:48:39 -0300

    create test map markers

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -8,10 +8,36 @@ describe('<Map />', () => {
 
 		expect(
 			screen.getByRole('link', {
-				name: /a js library for interactive maps/i
+				name: /a javascript library for interactive maps/i
 			})
 		).toBeInTheDocument()
 	})
 
-	it('should render with the marker in the correct place', () => {})
+	it('should render with the marker in the correct place', () => {
+		const places = [
+			{
+				id: '1',
+				name: 'Piracicaba',
+				slug: 'piracicaba',
+				location: {
+					latitude: -22.725,
+					longitude: -47.6476
+				}
+			},
+			{
+				id: '2',
+				name: 'Venâncio Aires',
+				slug: 'venancio-aires',
+				location: {
+					latitude: -29.606,
+					longitude: -52.1944
+				}
+			}
+		]
+
+		render(<Map places={places} />)
+
+		expect(screen.getByTitle(/piracicaba/i)).toBeInTheDocument()
+		expect(screen.getByTitle(/venâncio aires/i)).toBeInTheDocument()
+	})
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,26 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-	return <Map places={[]} />
+	const places = [
+		{
+			id: '1',
+			name: 'Piracicaba',
+			slug: 'piracicaba',
+			location: {
+				latitude: -22.725,
+				longitude: -47.6476
+			}
+		},
+		{
+			id: '2',
+			name: 'Venâncio Aires',
+			slug: 'venancio-aires',
+			location: {
+				latitude: -29.606,
+				longitude: -52.1944
+			}
+		}
+	]
+
+	return <Map places={places} />
 }
